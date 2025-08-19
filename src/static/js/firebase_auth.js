9e3dc61e0aa5b03b22548d60b606cf27fa7866b2
MARTIN bAER <216437053+MBAER-CODE@USERS.NOREPLY.GITHUB.COt config user.email>
2025-08-19 10:19:58 -0700

    removed hardcoded env variable values from firebase_auth.js

--- src/static/js/firebase_auth.js
+++ src/static/js/firebase_auth.js
@@ -5,13 +5,22 @@ import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } f
 
 // Your web app's Firebase configuration
 // These values have been filled in using your project details.
+#const firebaseConfig = {
+#   apiKey: "AIzaSyBIGGseFMifZwPk6RFuqs2ID7bKKASKF0w",
+#   authDomain: "work-schedule-cloud-36477.firebaseapp.com",
+#   projectId: "work-schedule-cloud-36477",
+#   storageBucket: "work-schedule-cloud-36477.appspot.com",
+#   messagingSenderId: "746093341275",
+#   appId: "YOUR_APP_ID_FROM_CONSOLE"
+#};
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBIGGseFMifZwPk6RFuqs2ID7bKKASKF0w",
-    authDomain: "work-schedule-cloud-36477.firebaseapp.com",
-    projectId: "work-schedule-cloud-36477",
-    storageBucket: "work-schedule-cloud-36477.appspot.com",
-    messagingSenderId: "746093341275",
-    appId: "YOUR_APP_ID_FROM_CONSOLE"
+   apiKey: process.env.FIREBASE_API_KEY,
+   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+   projectId: process.env.FIREBASE_PROJECT_ID,
+   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+   appId: process.env.FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
